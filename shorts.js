68bd3d5b35162301280d927ebfabcfb1b831a63c
Quema <myoon0929@gmail.com>
2024-07-09 21:24:45 +0900

    shorts auto skip

---
+++ shorts.js
@@ -0,0 +1,34 @@
+const auto = () => {
+    const currentTime = new Date().toLocaleTimeString();
+    console.log(`wait - ${currentTime}`);
+    setTimeout(() => {
+        const value = document.querySelector("ytd-scrubber > shorts-player-controls > div > div > div > div[aria-valuenow]");
+        console.log(value);
+        if (value == null || value == undefined) {
+            console.log("null");
+            const Ko = document.querySelector("button[aria-label='다음 동영상']");
+            const En = document.querySelector("button[aria-label='Next video']");
+            if (Ko) {
+                Ko.click();
+            }
+            if (En) {
+                En.click();
+            }
+        } else {
+            console.log("here");
+            if (parseInt(value.getAttribute('aria-valuenow'), 10) >= 97) {
+                const Ko = document.querySelector("button[aria-label='다음 동영상']");
+                const En = document.querySelector("button[aria-label='Next video']");
+                if (Ko) {
+                    Ko.click();
+                }
+                if (En) {
+                    En.click();
+                }
+            }
+        }
+        auto();
+    }, 1000);
+};
+
+window.onload = auto;
