7cb45366807974c6d8d8948ccd683feb4dfa7fc7
Quema <myoon0929@gmail.com>
2024-08-25 17:43:20 +0900

    fix time

--- shorts.js
+++ shorts.js
@@ -1,30 +1,34 @@
 const auto = () => {
     if (!window.location.href.startsWith("https://www.youtube.com/shorts/")) return
+
+    const ad = document.querySelector('div.badge-shape-wiz__text');
+    const adstyle = document.querySelector('.ytd-in-feed-ad-layout-renderer');
+
+    if(ad && adstyle){
+        if(ad.textContent === "스폰서" || ad.textContent === "Sponsored"){
+            console.log("adSkip");
+            ad.remove();
+            const Ko = document.querySelector("button[aria-label='다음 동영상']");
+            const En = document.querySelector("button[aria-label='Next video']");
+            if (Ko) {
+                Ko.click();
+            }
+            if (En) {
+                En.click();
+            }
+        }
+    }
+
     for (let i = 0; i < document.getElementsByTagName("video").length; i++) {
         const video = document.getElementsByTagName('video')[i];
         const slider = document.querySelector("ytd-scrubber > desktop-shorts-player-controls > div > yt-progress-bar > div[role='slider']");
-        const ad = document.querySelector('div[class="badge-shape-wiz__text"]')
         
         console.log('warking')
-        if(ad){
-            if(ad.textContent === "스폰서" || "Sponsored"){
-                console.log("adSkip");
-                ad.remove()
-                const Ko = document.querySelector("button[aria-label='다음 동영상']");
-                const En = document.querySelector("button[aria-label='Next video']");
-                if (Ko) {
-                    Ko.click();
-                    }
-                if (En) {
-                    En.click();
-                }
-            }
-        }
 
         if(slider){
             if (Number.isFinite(video.duration)) {
                 console.log(video.currentTime , video.duration)
-                if(video.currentTime >= video.duration-.3){
+                if(video.currentTime >= video.duration-.7){
                     console.log("Next");
                     const Ko = document.querySelector("button[aria-label='다음 동영상']");
                     const En = document.querySelector("button[aria-label='Next video']");
@@ -42,4 +46,4 @@ const auto = () => {
 
 window.onload = auto;
 
-setInterval(()=>{auto()},500)
+setInterval(()=>{auto()},700)
